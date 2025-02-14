4d7506aedbf2d386013f148aadfd055d0c8e8066
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-02-14 22:20:36 +0700

    add Course and Category models with initial migration

--- first_django_project/courses/models.py
+++ first_django_project/courses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+class Course(models.Model):
+    subject = models.CharField(max_length=100, unique=True)
+    decription = models.CharField(max_length=255, null=True)
+    created_date = models.DateTimeField(null=True)
+    uploaded_date = models.DateTimeField(null=True)
+    active = models.BooleanField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    
