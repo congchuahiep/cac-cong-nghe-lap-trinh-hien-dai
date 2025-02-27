e77b70b91a0d1428dd56eb0391285383015e17ed
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-02-27 10:46:06 +0700

    add Course REST API with serializer and viewset, and configure URLs

--- first_django_project/courses/views.py
+++ first_django_project/courses/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Course
+from .serializers import CourseSerializer
 
 # Create your views here.
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [permissions.IsAuthenticated]
