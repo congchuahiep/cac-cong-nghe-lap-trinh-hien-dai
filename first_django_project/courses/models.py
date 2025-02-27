4b3c303d6df9b6937fe31064271932eb74e5041f
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-02-27 10:27:58 +0700

    add __str__ method to Category model for better string representation

--- first_django_project/courses/models.py
+++ first_django_project/courses/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     
+    def __str__(self):
+        return self.name
+    
 class Course(models.Model):
     subject = models.CharField(max_length=100, unique=True)
     decription = models.CharField(max_length=255, null=True)
